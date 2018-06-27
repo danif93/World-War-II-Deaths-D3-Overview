a2fdcc1a65d01c591680e8faed598e43bc29c810
Gianluca Gambari <gianluca.gambari@outlook.it>
2018-06-27 19:58:05 +0200

    commit -- I resolve the problem about rect color.

--- js/script_comparison.js
+++ js/script_comparison.js
@@ -1,4 +1,49 @@
 var WWII_casualties
+
+
+function selectCountry () {
+  return d3.map(WWII_casualties, function(d){
+    return d.COUNTRY;
+  }).keys()
+}
+
+function setCountries () {
+
+  var countries = d3.select("#countries")
+  var svgBounds = countries.node().getBoundingClientRect();
+
+  var setColor = d3.scaleSequential(d3.interpolateRainbow)
+                   .domain([0,30])
+
+  console.log(selectCountry ());
+  console.log(setColor('Italy'));
+
+  var bar = d3.select("#rect")
+            .selectAll("rect")
+            .data([0,1,2,3,4,])
+            .enter()
+            .append("rect")
+            .attr("x", 10)
+            .attr("y", 20)
+            .attr("width", "10px")
+            .attr("height", "10px")
+            .attr("fill", function(d) {
+                            return setColor(d);
+                          })
+
+  var country = d3.select("#coun")
+                  .data(selectCountry())
+                  .enter()
+                  .append("text")
+                  .attr("x", 10)
+                  .attr("y", 20)
+                  .text(function(d){
+                    return d
+                  });
+
+
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////  LOAD FILE   //////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -17,43 +62,3 @@ d3.csv("../datasets/WWII_casualties.csv", function (error, csv_wwii) {
     setCountries();
 
 });
-
-function selectCountry (dataset) {
-  return d3.map(dataset, function(d){
-    return d.COUNTRY;
-  }).keys()
-}
-
-function setCountries () {
-
-  var countries = d3.select("#countries")
-  var svgBounds = countries.node().getBoundingClientRect();
-
-  var setColor = d3.scaleSequential(d3.interpolateRainbow)
-                   .domain([0,1,2,3,4,5,6])
-
-  console.log(setColor(6));
-
-
-
- var bar = d3.select("#rect")
-            .selectAll("rect")
-            .data(selectCountry(WWII_casualties))
-            .enter()
-            .append("rect")
-            .attr("x", "10px")
-            .attr("y", function(d) {
-                     return "20px"
-                  })
-            .transition()
-            .attr("width", "10px")
-            .attr("height", "10px")
-            .attr("fill", function(d) {
-                              //console.log(setColor.domain());
-                             return setColor(d);
-                           })
-            .text(function(d){
-              return d;
-            })
-
-}
