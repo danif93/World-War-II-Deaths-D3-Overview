797a6a4f6768a906b7091955696a014adb65dc9e
Danilo Franco <franco.danilo1993@gmail.com>
2018-07-11 16:59:18 +0200

    refactored and commented comparison;

--- js/script_comparison.js
+++ js/script_comparison.js
@@ -3,15 +3,16 @@ var months = {"1":"Jan", "2":"Feb", "3":"Mar", "4":"Apr", "5":"May", "6":"Jun",
               "7":"Jul", "8":"Aug", "9":"Sep", "10":"Oct", "11":"Nov", "12":"Dec"};
 var svgBounds = d3.select("#linechart").node().getBoundingClientRect();
 var xpad = 30;
-var ypad = 60;
+var bottomPad = 60;
+var topPad = 40;
 var colorScale;
 var xScale;
 var yScale = d3.scaleLinear()
-            .range([svgBounds.height-ypad, ypad-20])
+            .range([svgBounds.height-bottomPad, topPad])
             .domain([0, 100000]);
 var months_list = [];
 var LineGenerator = d3.line()
-                      .x(function(d) { return xScale(d.month) })
+                      .x(function(d) { return xScale(d.month); })
                       .y(function(d) { return yScale(d.deaths); });
 var stateDeaths = {};
 var currentMaxDeaths = 0;
@@ -43,10 +44,10 @@ window.onload = () => {
       colorScale = d3.scaleSequential(d3.interpolateRainbow)
                     .domain([0,distinctCountries().length-1])
 
-      warMonths.forEach(function(d){
+      warMonths.forEach(function(d) {
                         if (d.YEAR >= 1939)
                           months_list.push(d.MONTHS.substring(0,3)+" "+d.YEAR.substring(2,4));
-                      })
+                        })
 
       xScale = d3.scaleBand()
                   .range([xpad, svgBounds.width-xpad])
@@ -63,7 +64,6 @@ function distinctCountries() {
 }
 
 function setCountriesLegend () {
-
   var countries = d3.select("#countries")
 
   var newGr = countries.selectAll("g")
@@ -73,23 +73,19 @@ function setCountriesLegend () {
                         .attr("transform", function(d,i) { return "translate(0,"+i*20+")" })
 
   newGr.append("rect")
-       .attr("width", "18px")
-       .attr("height", "18px")
        .attr("fill", function(d, i) { return colorScale(i); })
-       .attr("opacity", 0.2)
+       .attr("opacity", 0.3)
        .attr("id", function(d, i){ return "r"+i })
 
   newGr.append("text")
       .attr("x", 30)
       .attr("dy", "1.05em")
       .text(function(d) { return d })
-      .attr("cursor", "pointer")
-      .on("click", function(d, i){
+      .on("click", function(d, i) {
+        // toggle
         d3.select("#r"+i).classed("selected", !d3.select("#r"+i).classed("selected"))
-        if (d3.select("#r"+i).classed("selected"))
-          addInfo(d, i)
-        else
-          removeInfo(d)
+        if (d3.select("#r"+i).classed("selected"))    addInfo(d, i)
+        else  removeInfo(d)
       })
 }
 
@@ -97,7 +93,7 @@ function createLineChart(){
   var svg = d3.select("#linechart");
 
   svg.select("#xAxis")
-      .attr("transform", "translate(-5,"+(svgBounds.height-ypad)+")")
+      .attr("transform", "translate(-5,"+(svgBounds.height-bottomPad)+")")
       .transition().duration(1000)
       .call(d3.axisBottom(xScale))
       .selectAll("text")
@@ -117,8 +113,8 @@ function createLineChart(){
     .append("line")
     .attr("x1", function(d) { return xScale(d); })
     .attr("x2", function(d) { return xScale(d); })
-    .attr("y1", ypad-20)
-    .attr("y2", svgBounds.height-ypad)
+    .attr("y1", topPad)
+    .attr("y2", svgBounds.height-bottomPad)
 }
 
 function getStateDeaths(state, colorIdx) {
@@ -127,39 +123,45 @@ function getStateDeaths(state, colorIdx) {
 
   var index = 0
 
+  // color and state variables inside the dictionary are just used for the tooltip filling
   months_list.forEach(function(m) { avgDeaths_list.push({"month":m, "deaths":0, "civ":0, "mil":0, "state":state, "color":colorIdx}) })
 
   WWII_casualties.forEach(function(d) {
-    if (d.COUNTRY == state /*&& d.TAGS != "air-firebomb"*/) {
+    if (d.COUNTRY == state) {
 
       var count_month = 0
 
+      // divide the string in group of 2 numbers
       var split_startdate = d.STARTDATE.match(/.{1,2}/g)
       var split_endate = d.ENDATE.match(/.{1,2}/g)
 
+      // how many yeat from startdate to enddate
       count_month += parseInt(split_endate[3])-parseInt(split_startdate[3])
-      if (count_month > 0) {
-        if (count_month > 1)
+      if (count_month > 0) { // if the period is not in the same year
+        if (count_month > 1) // if there's a full year in between
           count_month = (count_month - 1) * 12
-
+        // remaining months of the first year
         count_month += 13 - parseInt(split_startdate[1])
+        // passed months of the last year
         count_month += parseInt(split_endate[1])
       }
-      else{
-        count_month += parseInt(split_endate[1])-parseInt(split_startdate[1]) + 1
-      }
+      else // period in the same year
+        count_month += parseInt(split_endate[1])-parseInt(split_startdate[1]) + 1;
 
+      // we start counting from...
       var month = parseInt(split_startdate[1]);
       var year = parseInt(split_startdate[3])
 
       for (i=0; i<count_month; i++) {
-        avgDeaths_list.forEach(function(dict){
+        avgDeaths_list.some(function(dict) {
           if (dict.month == months[String(month)]+" "+String(year)) {
             dict.deaths += +d.DEATHSFINAL/count_month
             dict.civ += (+d.DEATHSFINAL/count_month)*(+d.CIVILIANRATE)
             dict.mil += +d.DEATHSFINAL/count_month*(1-(+d.CIVILIANRATE))
 
             if (dict.deaths > maxDeathValue) maxDeathValue = dict.deaths
+            // once found skip the remaining cicles
+            return dict.month === months[String(month)]+" "+String(year)
           }
         })
         month++
@@ -174,15 +176,14 @@ function getStateDeaths(state, colorIdx) {
 }
 
 function addInfo(state, colorIdx){
-  [avgDeaths_list, maxDeathValue] = getStateDeaths(state, colorIdx)
+  [avgDeaths_list, maxDeathValue] = getStateDeaths(state, colorIdx);
 
-
-  if (maxDeathValue > currentMaxDeaths) {
-     currentMaxDeaths = maxDeathValue
-     rescaleLinechart()
+  if (maxDeathValue > currentMaxDeaths) { // if the line exit the chart scope...
+     currentMaxDeaths = maxDeathValue;
+     rescaleLinechart();
   }
 
-  stateDeaths[state] = [avgDeaths_list, maxDeathValue]
+  stateDeaths[state] = [avgDeaths_list, maxDeathValue];
 
   d3.select("#lines").append("path")
                     .attr("id", "l"+state.replace(" ", ""))
@@ -195,18 +196,19 @@ function addInfo(state, colorIdx){
   d3.select("#points").append("g")
       .attr("id", "c"+state.replace(" ", ""))
       .selectAll("circle")
-      .data(avgDeaths_list).enter()
+      .data(avgDeaths_list)
+      .enter()
       .append("circle")
       .attr("cx", function(d) { return xScale(d.month); })
       .attr("cy", function(d) { return yScale(d.deaths); })
-      .attr("id", "c"+state)
       .attr("r", xScale.bandwidth()/2)
       .attr("fill", colorScale(colorIdx))
       .on("mouseover", function() {
                         var x = d3.event.pageX
                         var y = d3.event.pageY
-                        var elems = d3.selectAll(document.elementsFromPoint(d3.event.x, d3.event.y)).filter("circle")
                         var tt = d3.select("#tooltip")
+                        // select all the overlapping circles in that position
+                        var elems = d3.selectAll(document.elementsFromPoint(d3.event.x, d3.event.y)).filter("circle")
 
                         elems._groups[0].forEach(function(c){
                           tt.append("li")
@@ -238,7 +240,7 @@ function removeInfo(state){
 
   var delStateDeath = stateDeaths[state][1]
   delete stateDeaths[state]
-  if (currentMaxDeaths == delStateDeath) {
+  if (currentMaxDeaths == delStateDeath) { // if it's removed the highest line...
     deaths = Object.values(stateDeaths)
     if (deaths.length!=0) {
       currentMaxDeaths = d3.max(deaths, function(a) {return a[1]} )
